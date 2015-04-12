584dd2eb7e9d3924979b5d341091efbfe2abab5d
Michael Sprauer <Michael.Sprauer@sap.com>
2015-04-12 22:41:56 +0200

    Gemfile, style and readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+author = Author.create! username: "admin", email: "admin@local.host"
+Article.create! title: Lorem::Base.new(:words, 5).output,
+                author: author,
+                teaser: Lorem::Base.new(:words, 20).output,
+                content: Lorem::Base.new(:paragraphs, 5).output
